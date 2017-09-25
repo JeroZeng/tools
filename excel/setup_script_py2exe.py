691507d9a38262b4490168fecb55d5be7912ebb2
fzeng <jeromezeng@163.com>
2017-09-25 16:48:51 +0800

    Create EXE using py2exe for a Tkinter program.

--- excel/setup_script_py2exe.py
+++ excel/setup_script_py2exe.py
@@ -1,6 +1,17 @@
 from distutils.core import setup
 import py2exe
 
+# to use tkinter
+'''
+py2exe.build_exe.py2exe.old_prepare = py2exe.build_exe.py2exe.plat_prepare
+def new_prep(self):
+  self.old_prepare()
+  from _tkinter import TK_VERSION, TCL_VERSION
+  self.dlls_in_exedir.append('tcl{0}.dll'.format(TCL_VERSION.replace('.','')))
+  self.dlls_in_exedir.append('tk{0}.dll'.format(TK_VERSION.replace('.','')))
+py2exe.build_exe.py2exe.plat_prepare = new_prep
+'''
+
 setup(
     options = {'py2exe': {'bundle_files': 1}},
     console = [{
